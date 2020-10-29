7e4fc7b09a051c70d50880acc2e807932c99c185
kyu-suke <ykaneoka7@gmail.com>
2020-10-29 23:48:04 +0900

    deeplのresponse headerに content-security-policy: frame-ancestors 'self' が含まれてるから無理っぽいわあ

--- js.js
+++ js.js
@@ -8,7 +8,6 @@ $(function() {
     if (selectedStr.length < 1) {
       $('#depl-ext').remove()
     }
-    selectedStr = ''
     if(window.getSelection){  //selectionオブジェクト取得
       if(window.getSelection().toString() !== '' && window.getSelection().toString() !== '\n'){  //文章チェック
         $('body').append(`<div id="depl-ext" style="cursor:pointer; z-index:99999;font-size:20px;text-align:center; position: absolute; width: 25px; height: 25px; background: red; top: ${y-35}px; left: ${x+5}px;">D</div>`)
@@ -22,7 +21,12 @@ $(function() {
   });
 
   $('body').on('click', '#depl-ext', function(e){
-    console.log(selectedStr);
+    console.log('hoge')
+    console.log(selectedStr)
+    console.log('fuga')
+    const url = 'https://www.deepl.com/translator#en/ja/' + selectedStr
+    $('body').append(`<iframe src="${url}" style="cursor:pointer; z-index:99999;font-size:20px;text-align:center; position: absolute; top: ${y-35}px; left: ${x+5}px;"></iframe>`)
+    console.log(url)
   });
 });
 
